6309567f0ff4ec106b86823369ca81547c02c9c4
DX Macbook 05 <raabrera@up.edu.ph>
2026-05-28 19:19:03 +0800

    make it work again1

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome1');
+    return view('welcome12');
 });
 
 Route::get('/unsafe-user', function (Request $request) {
