8e4cc7db884b824155af03b7b287262d7cea1ce6
DX Macbook 05 <raabrera@up.edu.ph>
2026-05-28 13:56:51 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/unsafe-user', function (Request $request) {
+    $id = $request->query('id');
+    $user = DB::select("SELECT * FROM users WHERE id = " . $id); 
+    return response()->json($user);
 });
