9d6f19b6c8542692366406de4e7c777c0ef47fa5
DX Macbook 05 <raabrera@up.edu.ph>
2026-05-28 19:13:35 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// 11$test_leak = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX";
+// 111$test_leak = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX";
 
 Route::get('/unsafe-user', function (Request $request) {
     $id = $request->query('id');
